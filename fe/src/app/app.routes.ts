b95f5ffbd867a60dbdb12a3832247d94e828f303
Srey91 <118907587+Srey91@users.noreply.github.com>
2026-01-20 09:41:25 +0700

    Driver avatar, profile update

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DriverComponent } from '../driver/driver.component';
+import { DriverProfileComponent } from '../driver/driver-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DriverComponent },
+  { path: 'driver', component: DriverComponent },
+  { path: 'driver-profile', component: DriverProfileComponent },
+  { path: '', redirectTo: 'driver', pathMatch: 'full' }
+];
