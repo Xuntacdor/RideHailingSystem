a9169ad460dcc15c4dea059524aebad90d83281e
Xuntacdor <nhatquang1223@gmail.com>
2026-01-30 09:35:15 +0700

    Feat(UserManagement): Add new end point Find user by role

--- demo/src/main/java/com/mycompany/rideapp/repository/UserRepository.java
+++ demo/src/main/java/com/mycompany/rideapp/repository/UserRepository.java
@@ -1,7 +1,10 @@
 
 package com.mycompany.rideapp.repository;
 
+import java.util.List;
 import java.util.Optional;
+
+import com.mycompany.rideapp.enums.Role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,7 +20,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findByEmailWithRoles(@Param("email") String email);
 
-    @Query("SELECT u FROM User u WHERE u.id = :id")
-    Optional<User> findByIdWithRoles(@Param("id") String id);
-
+    @Query("SELECT u FROM User u WHERE " +
+           "(:role IS NULL OR u.role = :role) AND " +
+           "(:keyword IS NULL OR u.userName LIKE %:keyword% OR u.email LIKE %:keyword%)")
+    List<User> filterUsers(@Param("role") Role role, @Param("keyword") String keyword);
 }
